4b26cf249546f382cf17d4bb03ca5103571348bb
Kavishka <it20094218@my.sliit.lk>
2023-10-27 10:27:00 +0530

    update

---
+++ gunicorn_config.py
@@ -0,0 +1,6 @@
+workers = 4  # Adjust this value based on your server's resources
+bind = "0.0.0.0:8000"  # Bind to all available network interfaces
+
+# Optionally, set a log file
+accesslog = "/path/to/access.log"
+errorlog = "/path/to/error.log"
